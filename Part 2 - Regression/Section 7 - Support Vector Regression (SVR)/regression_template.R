db6be7352fd011ae8a15e94ffe7fbcac960b01b9
Anish Pillay <anishpillay2002@gmail.com>
2019-10-31 19:21:19 -0400

    linear regression complete

--- Part 2 - Regression/Section 7 - Support Vector Regression (SVR)/regression_template.R
+++ Part 2 - Regression/Section 7 - Support Vector Regression (SVR)/regression_template.R
@@ -5,7 +5,7 @@ dataset = read.csv('Position_Salaries.csv')
 dataset = dataset[2:3]
 
 # Splitting the dataset into the Training set and Test set
-# # install.packages('caTools')
+install.packages('e1071')
 # library(caTools)
 # set.seed(123)
 # split = sample.split(dataset$Salary, SplitRatio = 2/3)
